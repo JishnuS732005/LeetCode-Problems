912af7e55d77b7d4b7a24323cc7713b26236f828
Jishnu Srinivasan <145823703+JishnuS732005@users.noreply.github.com>
2025-08-02 12:03:13 +0530

    Time: 14 ms (31.76%) | Memory: 45.3 MB (19.70%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,26 +1,8 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        char ch[] = s.toLowerCase().toCharArray();
-        String str = "";
-        for(int i=0;i<s.length();i++) {
-            if(ch[i]>='a' && ch[i]<='z' || ch[i]>='0' && ch[i]<='9') {
-                str += ch[i];
-            }
-        }
-        System.out.print(str);
-        String rev = "";
-        for(int i=s.length()-1;i>=0;i--) {
-            if(ch[i]>='a' && ch[i]<='z' || ch[i]>='0' && ch[i]<='9') {
-                rev += ch[i];
-            }
-        }
-        System.out.println();
-        System.out.print(rev);
-        if(str.equals(rev)) {
-            return true;
-        }
-        else {
-            return false;
-        }
+       if(s == " ") return true;
+       s = s.replaceAll("[^a-zA-Z0-9]","").toLowerCase();
+       String str = new StringBuilder(s).reverse().toString();
+       return s.equals(str);
     }
 }
