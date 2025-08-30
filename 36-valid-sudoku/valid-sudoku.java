b03096e455c111de975b5ff63835f20ac824dac9
Jishnu Srinivasan <145823703+JishnuS732005@users.noreply.github.com>
2025-08-30 18:44:14 +0530

    Time: 1 ms (99.87%) | Memory: 44.8 MB (39.57%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.java
+++ 36-valid-sudoku/valid-sudoku.java
@@ -1,13 +1,18 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        Set<String> s = new HashSet<>();
+        boolean[][] r = new boolean[9][9];
+        boolean[][] c = new boolean[9][9];
+        boolean[][] b = new boolean[9][9];
         for(int i=0;i<9;i++) {
             for(int j=0;j<9;j++) {
-                char num = board[i][j];
-                if(num != '.') {
-                    if(!s.add(num+"in row"+i) || !s.add(num+"in col"+j) || !s.add(num+"in box"+(i/3)+"-"+(j/3))) {
+                if(board[i][j] != '.') {
+                    char v = board[i][j];
+                    int num = v-'1';
+                    int box = (i/3)*3 + j/3;
+                    if(r[i][num] || c[j][num] || b[box][num]) {
                         return false;
                     }
+                    r[i][num] = c[j][num] = b[box][num] = true;
                 }
             }
         }
