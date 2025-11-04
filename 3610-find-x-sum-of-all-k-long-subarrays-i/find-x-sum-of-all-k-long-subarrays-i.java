6b5cf7d54d885093ad7af5190f922ab68d2cabd3
Jishnu Srinivasan <145823703+JishnuS732005@users.noreply.github.com>
2025-11-04 19:50:24 +0530

    Time: 12 ms (55.17%) | Memory: 47.5 MB (7.59%) - LeetSync

--- 3610-find-x-sum-of-all-k-long-subarrays-i/find-x-sum-of-all-k-long-subarrays-i.java
+++ 3610-find-x-sum-of-all-k-long-subarrays-i/find-x-sum-of-all-k-long-subarrays-i.java
@@ -1,31 +1,28 @@
 class Solution {
-   public int[] findXSum(int[] nums, int k, int x) {
-	int n = nums.length;
-	int res[] = new int[n-k+1];
-	for(int i=0; i<res.length; i++) {
-		int sum = 0;
-		Set<Integer> set = new HashSet<>();
-		HashMap<Integer, Integer> map = new HashMap<>();
-		for(int j=i; j < i+k; j++) {
-			sum += nums[j];
-			set.add(nums[j]);
-			map.put(nums[j], map.getOrDefault(nums[j], 0) + 1);
-		}
+    public int[] findXSum(int[] nums, int k, int x) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int [] res = new int [nums.length - k + 1];
 
-		if(set.size() < x) res[i] = sum;
-		else {
-			PriorityQueue<Integer> pq = new PriorityQueue<>((a,b) -> {
-				if(map.get(a) == map.get(b)) return b-a;
-				return map.get(b)-map.get(a);
-			});
-			for(int ele : set) pq.add(ele);
-			int ct = x;
-			while(ct-- > 0) {
-				int top = pq.remove();
-				res[i] += (top*map.get(top));
-			}
-		}
-	}
-	return res;
-   }
+        for (int i = 0, j = 0, r = 0; j < nums.length; j++) {
+            map.put(nums[j], map.getOrDefault(nums[j], 0) + 1);
+
+            if (j >= k - 1) {
+                Queue<int []> q = new PriorityQueue<>((a,b) -> b[1] == a[1] ? b[0] - a[0] : b[1] - a[1]);
+                for (int key : map.keySet()) {
+                    q.add(new int [] {key, map.get(key)});
+                }
+
+                int m = x, sum = 0;
+
+                while (!q.isEmpty() && m-- > 0) {
+                    int [] a = q.remove();
+                    sum += a[0] * a[1];
+                }
+
+                res[r++] = sum;
+                map.put(nums[i], map.get(nums[i++]) - 1);
+            }
+        }
+        return res;
+    }
 }
